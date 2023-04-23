0d84ffebb9a3c469246eb8c0f0b691e051d49efa
AncFsh <119269965+AncFsh@users.noreply.github.com>
2023-04-24 01:11:43 +0200

    Update palindromy.py

--- palindromy.py
+++ palindromy.py
@@ -1,12 +1,7 @@
 def palindrome_check(word):
     return word == word[::-1]
 
-'Za pomocą tej funkcji program sprawdza czy po odwróceniu słowa, jest takie samo'
-
 word = input("Please provide the word: ")
 palindrome = palindrome_check(word)
 
-if palindrome:
-    print(True)
-else:
-    print(False)
+print(palindrome)
